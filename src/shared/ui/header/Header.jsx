f1d8ec2dd293275097daa2cb3e1dfe72993c0565
duddk <duddk@scatterx.io>
2025-06-30 12:45:25 +0900

    feat: header 검색창 추가

--- src/shared/ui/header/Header.jsx
+++ src/shared/ui/header/Header.jsx
@@ -1,13 +1,35 @@
+import { useState } from 'react'
+import { Link, useNavigate } from 'react-router'
+import { FiSearch } from "react-icons/fi";
 import styles from './Header.module.css'
 
 export function Header() {
+  const navigate = useNavigate();
+  const [text, setText] = useState('');
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    navigate(`/${text}`);
+  }
+
   return (
-    <div className={styles['logo-container']}>
-      <img
-        className={styles.logo} 
-        src="/public/youtube.png" 
-        alt="logo" />
-      <span>YoungTube</span>
-    </div>
+    <header>
+      <Link to='/' className={styles.logoContainer}>
+        <img
+          className={styles.logo} 
+          src="/public/youtube.png" 
+          alt="logo" />
+        <span className={styles.logoText}>YoungTube</span>
+      </Link>
+      <form onSubmit={handleSubmit}>
+        <input 
+          type="text"
+          placeholder='검색' 
+          value={text} 
+          onChange={(e) => setText(e.target.value)} 
+        />
+        <button type='submit'><FiSearch /></button>
+      </form>
+    </header>
   )
 }

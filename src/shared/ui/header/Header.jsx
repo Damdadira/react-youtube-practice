e6cd30a238da221befe0cea042a7ec6335f06cf6
duddk <duddk@scatterx.io>
2025-06-30 13:45:20 +0900

    style: header 검색창, avatar 스타일링

--- src/shared/ui/header/Header.jsx
+++ src/shared/ui/header/Header.jsx
@@ -2,34 +2,37 @@ import { useState } from 'react'
 import { Link, useNavigate } from 'react-router'
 import { FiSearch } from "react-icons/fi";
 import styles from './Header.module.css'
+import avatarImage from '../../assets/images/monster.png'
 
 export function Header() {
   const navigate = useNavigate();
   const [text, setText] = useState('');
-  
+
   const handleSubmit = (e) => {
     e.preventDefault();
     navigate(`/${text}`);
   }
 
   return (
-    <header>
+    <header className={styles.headerContainer}>
       <Link to='/' className={styles.logoContainer}>
         <img
           className={styles.logo} 
-          src="/public/youtube.png" 
-          alt="logo" />
+          src='/public/youtube.png' 
+          alt='logo' />
         <span className={styles.logoText}>YoungTube</span>
       </Link>
-      <form onSubmit={handleSubmit}>
+      <form className={styles.formContainer} onSubmit={handleSubmit}>
         <input 
-          type="text"
-          placeholder='검색' 
+          type='text'
+          placeholder='검색'
+          className={styles.formInput}
           value={text} 
           onChange={(e) => setText(e.target.value)} 
         />
-        <button type='submit'><FiSearch /></button>
+        <button type='submit' className={styles.formButton}><FiSearch /></button>
       </form>
+      <img className={styles.avatarImage} src={avatarImage} alt='avatar' />
     </header>
   )
 }

f16519331812cf5a98919e6f0b87c4224352207c
duddk <duddk@scatterx.io>
2025-07-03 17:04:16 +0900

    feat: ReplyList 컴포넌트 작업중

--- src/entities/comment/ui/ReplyItem/ReplyItem.jsx
+++ src/entities/comment/ui/ReplyItem/ReplyItem.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import { useYoutubeApi } from '../../../../app/context/YoutubeApiContext';
 import clsx from 'clsx';
 import styles from '../ReplyItem/ReplyItem.module.css';
+import ReplyList from '../ReplyList/ReplyList';
 
 export default function ReplyItem({ id, totalCount }) {
   const { youtube } = useYoutubeApi();
@@ -16,15 +17,18 @@ export default function ReplyItem({ id, totalCount }) {
     setToggle(!toggle);
   };
 
-  // console.log(replies)
-  // console.log(toggle)
-
   return (
     <>
       {totalCount > 0 && (
         <div className={styles.replyContainer}>
           <button onClick={handleClick}>답글 {totalCount}개</button>
-          <ul>답글 목록들~</ul>
+          {replies.length > 0 && (
+            <ul>
+              {replies.map((reply) => (
+                <ReplyList reply={reply}></ReplyList>
+              ))}
+            </ul>
+          )}
         </div>
       )}
     </>

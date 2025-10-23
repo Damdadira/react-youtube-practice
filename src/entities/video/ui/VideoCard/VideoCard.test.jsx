a1fcb857a71fcb63b975b75aa0bcb4a3def11447
duddk <duddk@scatterx.io>
2025-10-23 17:06:06 +0900

    feat: 비디오 카드 클릭 이벤트 테스트

--- src/entities/video/ui/VideoCard/VideoCard.test.jsx
+++ src/entities/video/ui/VideoCard/VideoCard.test.jsx
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router';
+import { MemoryRouter, Routes, Route, useLocation } from 'react-router';
 import { describe, expect, it } from 'vitest';
 import { formatAgo } from '../../../../shared/lib/timeago';
 import VideoCard from './VideoCard';
+import userEvent from '@testing-library/user-event';
 
 describe('VideoCard', () => {
   // 1. 테스트에 필요한 데이터와 환경 설정
@@ -21,6 +22,7 @@ describe('VideoCard', () => {
     },
   };
   const { title, channelTitle, publishedAt, thumbnails } = video.snippet;
+  // 정적
   it('renders video item', () => {
     // 2. 컴포넌트 렌더링(실행)
     render(
@@ -36,5 +38,27 @@ describe('VideoCard', () => {
     expect(screen.getByText(title)).toBeInTheDocument();
     expect(screen.getByText(channelTitle)).toBeInTheDocument();
     expect(screen.getByText(formatAgo(publishedAt, 'ko'))).toBeInTheDocument();
+  });
+
+  // 동적
+  it('navigates to detailed video page with video state when clicked', async () => {
+    function LocationStateDisplay() {
+      return <pre>{JSON.stringify(useLocation().state)}</pre>;
+    }
+    render(
+      <MemoryRouter initialEntries={['/']}>
+        <Routes>
+          <Route path="/" element={<VideoCard video={video} />}></Route>
+          <Route
+            path={`/watch/${video.id}`}
+            element={<LocationStateDisplay />}
+          ></Route>
+        </Routes>
+      </MemoryRouter>
+    );
+
+    const card = screen.getByRole('listitem');
+    await userEvent.click(card);
+    expect(screen.getByText(JSON.stringify({ video }))).toBeInTheDocument();
   });
 });

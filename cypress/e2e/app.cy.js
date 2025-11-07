c7ba0a6dd74ed406917503518c55039d3118b726
duddk <duddk@scatterx.io>
2025-11-07 12:47:44 +0900

    feat: popular video 테스트

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,10 +3,21 @@ import '@testing-library/cypress/add-commands'
 
 describe("Youtube App", () => {
   beforeEach(() => {
+    cy.intercept("GET", /(mostPopular)/g, {
+      fixture: "popular.json"
+    });
+    cy.intercept("GET", /(search)/g, {
+      fixture: "search.json"
+    });
+    cy.viewport(1200, 800);
     cy.visit("/")
   });
 
   it("renders", () => {
     cy.findByText("YoungTube").should("exist");
   });
+
+  it("shows popular video first", () => {
+    cy.findByText("Popular Video").should("exist");
+  })
 });

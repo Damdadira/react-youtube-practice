9d981379fa267bdc66ec2beafcf0c4a6628578bb
duddk <duddk@scatterx.io>
2025-11-07 13:17:45 +0900

    feat: 나머지 기능 테스트

--- cypress/e2e/app.cy.js
+++ cypress/e2e/app.cy.js
@@ -3,14 +3,19 @@ import '@testing-library/cypress/add-commands'
 
 describe("Youtube App", () => {
   beforeEach(() => {
-    cy.intercept("GET", /(mostPopular)/g, {
+    // alias 추가 및 정규식 g 플래그 제거
+    cy.intercept("GET", /(mostPopular)/, {
       fixture: "popular.json"
-    });
-    cy.intercept("GET", /(search)/g, {
+    }).as("popularAPI");
+    
+    cy.intercept("GET", /(search)/, {
       fixture: "search.json"
-    });
+    }).as("searchAPI");
+    
     cy.viewport(1200, 800);
-    cy.visit("/")
+    cy.visit("/");
+    
+    cy.findByText("YoungTube").should("exist");
   });
 
   it("renders", () => {
@@ -19,5 +24,20 @@ describe("Youtube App", () => {
 
   it("shows popular video first", () => {
     cy.findByText("Popular Video").should("exist");
-  })
+  });
+
+  it("searches by keyword", () => {
+    cy.findByPlaceholderText("검색").type("rose");
+    cy.findByRole("button", { name: "검색" }).click();
+    cy.findAllByRole("listitem", { timeout: 10000 }).should("have.length.gt", 0);
+    cy.contains("this happened at my surf competition", { timeout: 10000 }).should("be.visible");
+  });
+
+  it("goes to detail page", () => {
+    cy.findAllByRole("listitem").first().click();
+    cy.url().should("include", "/watch");
+    cy.findByText("Popular Video", { timeout: 10000 }).should("exist");
+    cy.get('iframe[id="player"]', { timeout: 10000 }).should("exist");
+    cy.contains("this happened at my surf competition", { timeout: 10000 }).should("be.visible");
+  });
 });
